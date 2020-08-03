1edcb218a0f5092900c8d65d1e816fd57589b8ea
root <root@DESKTOP-5JF94HV.localdomain>
2020-08-03 17:56:20 +0300

    First project commit

---
+++ Itinerary.java
@@ -0,0 +1,55 @@
+package Rail_IL;
+
+public class Itinerary {
+	
+	private String originStation,departureTime,ETA,destinationStation;
+
+	public Itinerary() {
+		// Empty on purpose - default constructor.
+	}
+
+	public String getOriginStation() {
+		return originStation;
+	}
+
+	public void setOriginStation(String originStation) {
+		this.originStation = originStation;
+	}
+
+	public String getDepartureTime() {
+		return departureTime;
+	}
+
+	public void setDepartureTime(String departureTime) {
+		this.departureTime = departureTime;
+	}
+
+	public String getETA() {
+		return ETA;
+	}
+
+	public void setETA(String eTA) {
+		ETA = eTA;
+	}
+
+	public String getDestinationStation() {
+		return destinationStation;
+	}
+
+	public void setDestinationStation(String destinationStation) {
+		this.destinationStation = destinationStation;
+	}
+
+	@Override
+	public String toString() {
+		return  originStation + " (" + departureTime + ") ----> " + destinationStation
+				+ " (" + ETA +").";
+	}
+	
+	
+	
+	
+	
+	
+
+}
