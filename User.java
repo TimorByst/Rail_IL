b12ce7d7cdb29f0d62343f94dd22228bfe7e8124
Dmitry Gribovsky <deman311@gmail.com>
2020-10-06 13:02:20 +0300

    adding user file

---
+++ User.java
@@ -0,0 +1,28 @@
+public class User {
+	public static void main(String args[]) {
+		Rail.findTripByDeparture(args[2], args[3], args[1], args[4]);
+		
+		System.out.println("<table style=\"height: 195px; border-color: black; background-color: pink; margin-left: auto; margin-right: auto;\" border=\"black\" width=\"662\">\r\n" + 
+				"<tbody>\r\n" + 
+				"<tr>\r\n" + 
+				"<td style=\"width: 158px;\"><strong>" + args[1] + "</strong></td>\r\n" + 
+				"<td style=\"width: 158px;\">" + args[2] + "</td>\r\n" + 
+				"<td style=\"width: 159px;\">" + args[3] + ":" + args[4] + "</td>\r\n" + 
+				"<td style=\"width: 159px;\">TEST</td>\r\n" + 
+				"</tr>\r\n" + 
+				"<tr>\r\n" + 
+				"<td style=\"width: 158px;\">Fuck you</td>\r\n" + 
+				"<td style=\"width: 158px;\">AAAA</td>\r\n" + 
+				"<td style=\"width: 159px;\">&nbsp;GO TO ----&gt;</td>\r\n" + 
+				"<td style=\"width: 159px;\">&nbsp;</td>\r\n" + 
+				"</tr>\r\n" + 
+				"<tr>\r\n" + 
+				"<td style=\"width: 158px;\">&nbsp;</td>\r\n" + 
+				"<td style=\"width: 158px;\">&nbsp;<img src=\"https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\" /><img src=\"https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\" /><img src=\"https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\" /><img src=\"https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\" /><img src=\"https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\" /><img src=\"https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\" /><img src=\"https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\" /></td>\r\n" + 
+				"<td style=\"width: 159px;\">ZZZ</td>\r\n" + 
+				"<td style=\"width: 159px;\">&nbsp;</td>\r\n" + 
+				"</tr>\r\n" + 
+				"</tbody>\r\n" + 
+				"</table>");
+	}
+}
